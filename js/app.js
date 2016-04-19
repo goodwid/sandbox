ab0cc3e19ed4573833d6fd1f9c773c4ff291122c
goodwid <goodwid@gmail.com>
2016-04-19 13:52:55 -0700

    functionl genratrs

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
+function lt_gen (num) {
+  return (function (check) {
+    return check < num;
+    })
+  };
 
+function gt_gen (num) {
+  return (function (check) {
+    return check > num;
+  });
+}
 
-var data=[];
+arr = [0,1,2,3,4,5,6,7,8,9,10];
 
-console.log(localStorage.getItem('data'));
-
-data.push('foo');
-data.push('bar');
-
-
-localStorage.setItem('data2',data);
-
-console.log(data);
+console.log(arr.filter(lt_gen(5)));
+console.log(arr.filter(gt_gen(4)));
