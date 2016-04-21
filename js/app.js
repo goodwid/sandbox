070c6c278a70e97cf99fb60970a371d56f86b3a6
goodwid <goodwid@gmail.com>
2016-04-21 10:51:46 -0700

    Created function taht returns an object w/ methods of various comparisons

--- js/app.js
+++ js/app.js
@@ -1,16 +1,26 @@
-function lt_gen (num) {
-  return (function (check) {
+function compareGen (num) {
+  return {
+    lt: function (check) {
     return check < num;
-    })
+    },
+    gt: function (check) {
+      return check > num;
+    },
+    lteq: function (check) {
+      return check <= num;
+    },
+    gteq: function (check) {
+      return check >= num;
+    },
+    eq: function (check) {
+      return check === num;
+    }
   };
-
-function gt_gen (num) {
-  return (function (check) {
-    return check > num;
-  });
 }
 
 arr = [0,1,2,3,4,5,6,7,8,9,10];
 
-console.log(arr.filter(lt_gen(5)));
-console.log(arr.filter(gt_gen(4)));
+console.log(arr.filter(compareGen(4).lt));
+console.log(arr.filter(compareGen(5).gt));
+console.log(arr.filter(compareGen(7).gteq));
+console.log(arr.filter(compareGen(2).eq));
