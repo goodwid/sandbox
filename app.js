635d63f53b10c9b50d5609b4e0ba788e1fdef12f
goodwid <goodwid@gmail.com>
2016-05-12 11:53:38 -0700

    Yeah

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const fs = require ('fs');
-const buf = Buffer.alloc(1024, 'Bob\'s your uncle!  ','utf-8' );
+const buf = Buffer.alloc(1024, 0xff);
 
 
 fs.appendFile('foo',buf, err => {if (err) throw err;});
