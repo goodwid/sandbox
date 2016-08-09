9bcf2e957ef28fcbc8347a5eb2db2a8f6f25c29e
David <goodwid@gmail.com>
2016-08-09 14:27:40 -0700

    Stuff

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-cats = [
+const cats = [
   ['Alice','images/cat1.jpg'],
   ['Bob','images/cat2.jpg'],
   ['Carol','images/cat3.jpg'],
   ['Dave','images/cat4.jpg'],
   ['Edna','images/cat5.jpg']
 ];
-var path = 'images/';
+
 var $cat = $('#cat');
 var $info = $('#info');
 var current = 0;
@@ -23,7 +23,7 @@ function changeImage(num) {
   current += num;
   if (current > cats.length -1) {
     current = 0;
-  };
+  }
   if (current <= -1) {
     current = cats.length -1;
   }
